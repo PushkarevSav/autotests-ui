37393da38cba5520b0e93d1816be12b6100da8bf
PushkarevSav <pespizdec@yandex.ru>
2025-08-09 21:07:12 +0700

    add context

--- tests/playwright_registration.py
+++ tests/playwright_registration.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
+
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
