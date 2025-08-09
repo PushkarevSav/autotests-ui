4812dba8ae34db127597de80982d2087092fcd8a
PushkarevSav <pespizdec@yandex.ru>
2025-08-09 20:52:53 +0700

    add context

--- tests/playwright_registration.py
+++ tests/playwright_registration.py
@@ -1,9 +1,9 @@
 from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
-
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
@@ -16,12 +16,15 @@ with sync_playwright() as playwright:
     password_registration = page.get_by_test_id('registration-form-password-input').locator('input')
     password_registration.fill('password')
 
-    button_registration = page.get_by_test_id('registration-page-registration-button')
-    button_registration.click()
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
 
-    dashoboard_toolbar = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashoboard_toolbar).to_be_visible()
-    expect(dashoboard_toolbar).to_have_text('Dashboard')
+    context.storage_state(path='browser-state.json')
 
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
 
-
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+    page.wait_for_timeout(5000)
