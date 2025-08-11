6cbb5284a911cfe1c2e8ac7beda4035565ed364b
PushkarevSav <pespizdec@yandex.ru>
2025-08-11 12:29:57 +0700

    7.1 Знакомство с PageObject Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,26 +1,18 @@
-from playwright.sync_api import expect, Page
 import pytest
+
+from fixtures.pages import dashboard_page
+from pages.registration_page import RegistrationPage
+from pages.dashborad_page import DashboardPage
+
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page: Page):
+@pytest.mark.parametrize('email, username, password', [('user.name@gmail.com', 'username', 'password')])
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email: str,
+                                 username: str, password: str):
 
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.fill_registration_form(email=email, username=username, password=password)
+    registration_page.click_registration_button()
 
-        chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-        email_registration = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-        email_registration.fill('user.name@gmail.com')
-
-        username_registration = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-        username_registration.fill('username')
-
-        password_registration = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-        password_registration.fill('password')
-
-        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-        expect(dashboard_title).to_be_visible()
-
-
+    dashboard_page.check_title_text_dashboard()
