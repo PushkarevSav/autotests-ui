f713e41ebc618c39a1aba820ea728068140be362
PushkarevSav <pespizdec@yandex.ru>
2025-09-05 01:22:13 +0600

    7.3 Знакомство с PageComponent Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -7,20 +7,17 @@ from pages.create_course_page import CreateCoursePage
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
-    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-    title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(title).to_have_text('Courses')
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
 
-    icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(icon).to_be_visible()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
-    empty_result_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(empty_result_text).to_have_text('There is no results')
 
-    description_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(description_text).to_have_text('Results from the load test pipeline will be displayed here')
 
 
 @pytest.mark.courses
