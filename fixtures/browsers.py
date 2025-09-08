670c72e0d58b2de9757f843e5d7cf5e64ba88145
PushkarevSav <pespizdec@yandex.ru>
2025-09-08 16:25:56 +0600

    7.6 Практические применение PageFactory в автотестах Практикуемся в применении Page Factory

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,44 +1,34 @@
 import pytest
-from playwright.sync_api import  Page, Playwright
+from playwright.sync_api import Playwright, Page
+
+from pages.authentication.registration_page import RegistrationPage
 
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
 
-         browser = playwright.chromium.launch(headless=False)
-         yield browser.new_page()
-         browser.close()
 
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
 
-@pytest.fixture(scope='session')
-def initialize_browser_state(playwright: Playwright) -> None:
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.click_registration_button()
 
-        browser = playwright.chromium.launch(headless=True)
-        context = browser.new_context()
-        page = context.new_page()
-
-        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-
-        email_registration = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_registration.fill('user.name@gmail.com')
-
-        username_registration = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_registration.fill('username')
-
-        password_registration = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_registration.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        context.storage_state(path='browser-state.json')
-        browser.close()
+    context.storage_state(path="browser-state.json")
+    browser.close()
 
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
-
-       browser = playwright.chromium.launch(headless=False)
-       context = browser.new_context(storage_state='browser-state.json')
-       yield context.new_page()
-       browser.close()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    yield context.new_page()
+    browser.close()
--- tests/test_pytest/playwright_disabled.py
+++ tests/test_pytest/playwright_disabled.py
@@ -1,7 +1,5 @@
 from playwright.sync_api import sync_playwright, expect
 
-from tests.test_authorization import login_button
-
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch()
     page = browser.new_page()
