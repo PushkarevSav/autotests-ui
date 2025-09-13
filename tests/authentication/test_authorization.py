2cb856135f59da743b204426db401a631a42b801
PushkarevSav <pespizdec@yandex.ru>
2025-09-13 16:30:45 +0600

    7.7 Наращивание тестовой базы и рефакторинг Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,13 +1,22 @@
 import pytest
 import allure
+from allure_commons.types import Severity
 
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashborad_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
+@allure.epic(AllureEpic.LMS) # Добавили epic
+@allure.feature(AllureFeature.AUTHENTICATION) # Добавили feature
+@allure.story(AllureStory.AUTHORIZATION) # Добавили story # Используем enum
 class TestAuthorization:
     @pytest.mark.parametrize(
         "email, password",
@@ -18,12 +27,18 @@ class TestAuthorization:
         ]
     )
     @allure.title("User login with wrong email or password")
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.severity(Severity.CRITICAL)
     def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
         login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
         login_page.login_form.fill(email=email, password=password)
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
 
+    @allure.title("User login with correct email and password")
+    @allure.tag(AllureTag.USER_LOGIN) # Используем enum
+    # Добавили заголовок
+    @allure.severity(Severity.BLOCKER) # Добавили severity
     def test_successful_authorization(
             self,
             login_page: LoginPage,
@@ -47,6 +62,9 @@ class TestAuthorization:
         dashboard_page.navbar.check_visible("username")
         dashboard_page.sidebar.check_visible()
 
+    @allure.title("Navigation from login page to registration page")
+    @allure.tag(AllureTag.NAVIGATION)
+    @allure.severity(Severity.NORMAL)
     def test_navigate_from_authorization_to_registration(
             self,
             login_page: LoginPage,
