d515090777721b93ab16558b05768eefa5136dab
PushkarevSav <pespizdec@yandex.ru>
2025-09-13 16:53:32 +0600

    8.2 Интеграция Allure в автотесты Практическая работа с Allure аннотациями

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -13,10 +13,15 @@ from tools.allure.tags import AllureTag
 
 @pytest.mark.regression
 @pytest.mark.authorization
+
 @allure.tag(AllureTag.REGRESSION, AllureTag.AUTHORIZATION)
-@allure.epic(AllureEpic.LMS) # Добавили epic
-@allure.feature(AllureFeature.AUTHENTICATION) # Добавили feature
-@allure.story(AllureStory.AUTHORIZATION) # Добавили story # Используем enum
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION) #
+@allure.story(AllureStory.AUTHORIZATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.AUTHORIZATION)
+
 class TestAuthorization:
     @pytest.mark.parametrize(
         "email, password",
